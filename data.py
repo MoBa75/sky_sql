35cc5d985cd1130e7c1fa75ebda40b90a4f13a7b
MoBa75 <barison@gmx.de>
2025-05-21 15:03:12 +0200

    added functionality to get flights by date and delay flights by airline and airport

--- data.py
+++ data.py
@@ -3,6 +3,19 @@ from sqlalchemy import create_engine, text
 QUERY_FLIGHT_BY_ID = text("SELECT flights.*, airlines.airline, flights.ID as FLIGHT_ID, "
                           "flights.DEPARTURE_DELAY as DELAY FROM flights JOIN airlines "
                           "ON flights.airline = airlines.id WHERE flights.ID = :id")
+QUERY_FLIGHT_BY_DATE = text("SELECT ID, ORIGIN_AIRPORT, DESTINATION_AIRPORT, AIRLINE, "
+                            "DEPARTURE_DELAY AS DELAY FROM flights WHERE YEAR = :year "
+                            "AND MONTH = :month AND DAY = :day")
+QUERY_FLIGHT_BY_AIRLINE = text("SELECT f.AIRLINE AS ID, f.ORIGIN_AIRPORT AS "
+                               "ORIGIN_AIRPORT, f.DESTINATION_AIRPORT AS DESTINATION_AIRPORT, "
+                               "a.AIRLINE AS AIRLINE, f.DEPARTURE_DELAY AS DELAY "
+                               "FROM flights AS f JOIN airlines AS a ON f.AIRLINE = a.ID "
+                               "WHERE a.AIRLINE = :airline AND f.DEPARTURE_DELAY >= 20")
+QUERY_FLIGHT_BY_AIRPORT = text("SELECT f.ID AS ID, f.ORIGIN_AIRPORT AS ORIGIN_AIRPORT, "
+                               "f.DESTINATION_AIRPORT AS DESTINATION_AIRPORT, "
+                               "a.AIRLINE AS AIRLINE, f.DEPARTURE_DELAY AS DELAY "
+                               "FROM flights AS f JOIN airlines AS a ON f.AIRLINE = a.ID "
+                               "WHERE f.ORIGIN_AIRPORT = :airport AND f.DEPARTURE_DELAY >= 20")
 
 class FlightData:
     """
@@ -34,6 +47,26 @@ class FlightData:
         """
         params = {'id': flight_id}
         return self._execute_query(QUERY_FLIGHT_BY_ID, params)
+
+
+    def get_flights_by_date(self, flight_day, flight_month, flight_year):
+        """
+        """
+        params = {'year': flight_year, 'month': flight_month, 'day': flight_day}
+        return self._execute_query(QUERY_FLIGHT_BY_DATE, params)
+
+    def get_delayed_flights_by_airline(self, airline_input):
+        """
+        """
+        params = {'airline': airline_input}
+        return self._execute_query(QUERY_FLIGHT_BY_AIRLINE, params)
+
+    def get_delayed_flights_by_airport(self, airport_input):
+        """
+        """
+        params = {'airport': airport_input}
+        return self._execute_query(QUERY_FLIGHT_BY_AIRPORT, params)
+
 
     def __del__(self):
         """
